cbb3045e582da1aedac7bfbd4fd130818146b11f
han201905 <63267166+han201905@users.noreply.github.com>
2021-06-01 23:54:29 -0700

    Wrote second test, check if 2100 returns False

--- leapyear_test.py
+++ leapyear_test.py
@@ -1,11 +1,14 @@
 import unittest
+from unittest import result
 import leapyear
 
 class Test(unittest.TestCase):
     def test_leap(self):
         result = leapyear.is_leap(2004)
         self.assertEqual(result, True)
-
+    def test_2(self):
+        result = leapyear.is_leap(2100)
+        self.assertEqual(result, False)
 
 if __name__ == '__main__':
     unittest.main()
