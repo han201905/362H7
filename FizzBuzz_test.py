fbb0cc6daa5cbb02dbd1da9fb9343060aed6f340
han201905 <63267166+han201905@users.noreply.github.com>
2021-06-01 23:01:42 -0700

    Wrote second test, check if 25 returns Buzz

--- FizzBuzz_test.py
+++ FizzBuzz_test.py
@@ -1,10 +1,15 @@
 import unittest
+from unittest import result
 import FizzBuzz
 
 class Test(unittest.TestCase):
     def test_false(self):
         result = FizzBuzz.fizzBuzz(9)
         self.assertEqual(result, "Fizz")
+    
+    def test_false(self):
+        result = FizzBuzz.fizzBuzz(25)
+        self.assertEqual(result, "Buzz")
 
 
 if __name__ == '__main__':
