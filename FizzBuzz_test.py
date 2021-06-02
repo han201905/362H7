aab34119fe2f505bb89da95d77b976f4a0000850
han201905 <63267166+han201905@users.noreply.github.com>
2021-06-01 23:12:35 -0700

    Wrote third test, check if 30 returns FizzBuzz

--- FizzBuzz_test.py
+++ FizzBuzz_test.py
@@ -3,13 +3,17 @@ from unittest import result
 import FizzBuzz
 
 class Test(unittest.TestCase):
-    def test_false(self):
+    def test_1(self):
         result = FizzBuzz.fizzBuzz(9)
         self.assertEqual(result, "Fizz")
     
-    def test_false(self):
+    def test_2(self):
         result = FizzBuzz.fizzBuzz(25)
         self.assertEqual(result, "Buzz")
+
+    def test_3(self):
+        result = FizzBuzz.fizzBuzz(30)
+        self.assertEqual(result, "FizzBuzz")
 
 
 if __name__ == '__main__':
